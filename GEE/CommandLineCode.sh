62efa1cadda2a4af64017c56e59d5dfc20251906
Leila <lfouda@gmail.com>
2023-07-20 12:06:19 -0400

    Update CommandLineCode.sh

--- GEE/CommandLineCode.sh
+++ GEE/CommandLineCode.sh
@@ -8,27 +8,34 @@ pip install earthengine-api
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- #
 # Upload data to google cloud storage and earth engine
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- #
+
+# location of CSV to upload
 csv=/Users/leila/projects/test-project/out/gee_test/testdata_copy.csv #location of csv to upload on computer
 
+# location of folder in GCS bucket
 gcs_csv_path=gs://BUCKETNAME/leila/projects/test_data_GEE/ #location to send data to in bucket
 
-#sends code to GCS
+# run gsutil to send data to GCS
 gsutil -q cp -r $csv $gcs_csv_path
 
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- #
 # Sends code to GEE form GCS
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- #
+
+# locatioin of csv in GCS
 gcsCSV=gs://BUCKETNAME/leila/projects/test_data_GEE/testdata_copy.csv
 
+# location for csv in GEE
 geePts=projects/gee-test-project/assets/gee-test-project-folder/testdata_copy
 
-#sends code to earth engine
+# run earth engine uploader (need to have installed on CLI)
 earthengine upload table $gcsCSV --asset_id $geePts --x_column lon --y_column lat --force
 
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- #
 # Run gee_anno (adapted from Ben Carlson - https://github.com/benscarlson/mosey_env/blob/main/main/gee_anno.r)
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- #
-#Then I ran gee_anno through the CL with:
+
+#Then run gee_anno (https://github.com/leilafouda/Protected-Area-Movement/blob/main/analysis/src/workflow/gee_anno_updated.R) (modified from: https://github.com/benscarlson/mosey_env/blob/main/main/gee_anno.r) through the CL with:
 #gee_anno.r <dat> <out> [--groups=<groups>] [--npts=<npts>] [--seed=<seed>] 
 
 #Parameters:

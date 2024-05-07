ad8a82db4994e6cb2d8af1e301d9611c41407e84
Leila <lfouda@gmail.com>
2024-05-07 10:31:57 -0300

    Update quick_tips.r

--- r/quick_tips.r
+++ r/quick_tips.r
@@ -8,3 +8,11 @@ test <- read.csv("This is a really long file name that will not fit within one l
 test_paste <- read.csv(paste0("This is a really lone file name that will not fit ",
                         "within one line because it is intentionally really ",
                         "long.csv"))
+
+#To filter out multiple items in a column
+filter(COLUMN %in%  c('ID1','ID2','ID3'))
+
+#e.g.
+dataframe_02 <- dataframe_01 %>%
+  filter(Column_01 %in% c('Name_02', 
+                          'Name_06', 'Name_08'))

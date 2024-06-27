cd25e515fd32c50a492bb39fb261a38fe8681736
Leila <lfouda@gmail.com>
2024-06-27 16:28:37 -0300

    Create time_in_r.r

---
+++ r/time_in_r.r
@@ -0,0 +1,6 @@
+#Working with tume in r
+#for setting time when you have a seconds since data:
+as.POSIXct(time, origin = "1970-01-01", tz="UTC")
+
+#Time Zones
+https://en.wikipedia.org/wiki/List_of_tz_database_time_zones

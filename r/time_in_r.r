c286492b6172f5f45acf2663c73ec37ad713fb68
Leila <lfouda@gmail.com>
2024-06-27 16:29:55 -0300

    Update time_in_r.r

--- r/time_in_r.r
+++ r/time_in_r.r
@@ -3,4 +3,4 @@
 as.POSIXct(time, origin = "1970-01-01", tz="UTC")
 
 #Time Zones
-[https://en.wikipedia.org/wiki/List_of_tz_database_time_zones](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones).
+https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
